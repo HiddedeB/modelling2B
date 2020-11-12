84373e3c62e6fdf82789b220227c6fa4044ffa04
Hidde de Bos <h.j.e.debos@student.tudelft.nl>
2020-11-12 17:02:50 +0100

    Updated PlanetClass.py
    working on basic functionality

--- PlanetClass.py
+++ PlanetClass.py
@@ -7,18 +7,20 @@ class Planet:
     def __init__(self,
                  initial_position,
                  mass,
-                 radius):
-        """"NOTE:
+                 radius,
+                 initial_velocity):
+        '''NOTE:
         :param initial_position: initial position vector
         :type initial_position: array
         :param float mass: mass of the planet
         :type mass: float
         :param float radius: radius of the planet
         :type radius: float
-        """
+        '''
         self._pos = initial_position
         self.mass = mass
         self.radius = radius
+        self._velocity = initial_velocity
 
     @property
     def pos(self):
@@ -27,3 +29,11 @@ class Planet:
     @pos.setter
     def pos(self, updated_pos):
         self._pos = updated_pos
+
+    @property
+    def velocity(self):
+        return self._velocity
+
+    @velocity.setter
+    def velocity(self, updated_velocity):
+        return self._velocity = updated_velocity
