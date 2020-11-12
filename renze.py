27846f1f4991f3f68fc81f5419c136e2ad6c124f
RenzeD <guitarenze@hotmail.nl>
2020-11-12 12:58:47 +0100

    a

--- renze.py
+++ renze.py
@@ -1,3 +1,14 @@
 from numba import jit
 import numpy as np
 from multiprocessing import Process as P
+from itertools import combinations
+
+objs = np.array([])
+
+#c'vo
+#public static dict MakeTree(lst):
+	#gebruiken we denk ik niet bij ellipsbanen?
+#	return 1
+
+for tijdstap:
+	pairs = np.array([i for i in combinations(objs,2)])
