3e82d6a7973966560c7beadbf4a7d463fee62f71
RenzeD <guitarenze@hotmail.nl>
2020-11-15 22:24:46 +0100

    createplanet

--- data.py
+++ data.py
@@ -19,46 +19,45 @@ class PlanetaryDataHandler:
 					eccentricity=j['eccentricity'], smaxis=j['smaxis'],argperiapsis=j['argperiapsis'],
 					orbital_inclination=j['orbital inclination'],mean_longitude=j['mean longitude'])
 			else:
-				temp = planet(mass=j['mass'],radius=j['radius'])
+				temp = planet(mass=j['mass'],radius=j['radius'], name=i)
 			setattr(self,i,temp)
 
 	def get_planets(self) -> np.ndarray:
 		return np.array([getattr(self,i) for i in self.rawdata])
 
-	def createnewplanet(self,_mass:int=0,_radius:int=0,_initial_position:np.ndarray=np.array([]),
-		_initial_velocity:np.ndarray=np.array([]),_loanode:float=0,_period:float=0,_name:str='Planet9',
-		_eccentricity:float=0,_smaxis:int=0,_argperiapsis:float=0,_orbital_inclination:float=0,_mean_longitude:float=0) -> bool:
+	def createnewplanet(self,mass:int=0,radius:int=0,initial_position:np.ndarray=np.array([]),
+		initial_velocity:np.ndarray=np.array([]),loanode:float=0,period:float=0,name:str='Planet9',
+		eccentricity:float=0,smaxis:int=0,argperiapsis:float=0,orbital_inclination:float=0,mean_longitude:float=0) -> bool:
 
 		"""
 		Function to create a new planet, 'Planet9' is used as default name. Returns True on succes
 		"""
 
-		if not _mass and _radius:
+		if not mass or not radius:
 			raise ValueError('No mass or radius was specified, try again.')
-		check1 = _loanode and _eccentricity and _smaxis and _argperiapsis and _orbital_inclination
-		check2 = _initial_position and _initial_velocity
-		if not check1:
-			warnings.warn('Not enough data specified to use this planet in Kepler coordinates.')
+		check1 = smaxis>0
+		check2 = initial_position==np.array([])
+		if not check1 and not check2:
+			raise ValueError('Not enough data specified to use this planet in simulations.')
 		elif not check2:
 			warnings.warn('Not enough data specified to use this planet in the ordinary motion equation.')
-		elif not check1 and not check2:
-			raise ValueError('Not enough data specified to use this planet in simulations.')
-		else:
-			if check1:
-				self.rawdata[_name] = {"mass":_mass,"radius":_radius,"loanode":_loanode,"eccentricity":_eccentricity,"smaxis":_smaxis,
-					"argperiapsis":_argperiapsis, "orbital inclination":_orbital_inclination,"mean_longitude":_mean_longitude}
-				j = self.rawdata[_name]
-				temp = planet(mass=j['mass'], radius=j['radius'], loanode=j['loanode'], period=j['period'], name=_name,
-					eccentricity=j['eccentricity'], smaxis=j['smaxis'],argperiapsis=j['argperiapsis'],
-					orbital_inclination=j['orbital inclination'],mean_longitude=j['mean longitude'])
-				setattr(self,_name,temp)
-				return True
-			elif check2:
-				self.rawdata[_name] = {"mass":_mass,"radius":_radius,"initial velocity":_initial_velocity,"initial position":_initial_position}
-				j = self.rawdata[_name]
-				temp = planet(mass=j['mass'],radius=j['radius'],initial_velocity=j['initial velocity'],initial_position=j['initial position'])
-				setattr(self,_name,temp)
-				return True
+		elif not check1:
+			warnings.warn('Not enough data specified to use this planet in Kepler coordinates.')
+		if check1:
+			self.rawdata[name] = {"mass":mass,"radius":radius,"loanode":loanode,"eccentricity":eccentricity,"smaxis":smaxis,
+				"argperiapsis":argperiapsis, "orbital inclination":orbital_inclination,"mean longitude":mean_longitude,"period":period}
+			j = self.rawdata[name]
+			temp = planet(mass=j['mass'], radius=j['radius'], loanode=j['loanode'], period=j['period'], name=name,
+				eccentricity=j['eccentricity'], smaxis=j['smaxis'],argperiapsis=j['argperiapsis'],
+				orbital_inclination=j['orbital inclination'],mean_longitude=j['mean longitude'])
+			setattr(self,name,temp)
+			return True
+		elif check2:
+			self.rawdata[name] = {"mass":mass,"radius":radius,"initial velocity":initial_velocity,"initial position":initial_position}
+			j = self.rawdata[name]
+			temp = planet(mass=j['mass'],radius=j['radius'],initial_velocity=j['initial velocity'],initial_position=j['initial position'])
+			setattr(self,name,temp)
+			return True
 
 	def __str__(self):
 		return "Please don't print me, ask for my rawdata instead. I'll help you a bit though. \n" + str(self.rawdata)
