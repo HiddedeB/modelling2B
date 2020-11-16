9ee335faeae94b0d44e8af86340414b70988284d
Hidde de Bos <h.j.e.debos@student.tudelft.nl>
2020-11-16 15:15:03 +0100

    Simulation updates

--- simulation.py
+++ simulation.py
@@ -1,21 +1,67 @@
+import scipy as sc
 import numpy as np
-import scipy.integrate as solve_ivp
-from PlanetClass import planet
+from scipy.integrate import solve_ivp
 import astropy.constants as const
+import matplotlib.pyplot as plt
+from numba import njit
+import copy
+from data import PlanetaryDataHandler
+pdh = PlanetaryDataHandler
 
-#important constants
 
-massa_zon = 1.989 * 10**(30) #kg, massa zon
-massa_aarde = 5.972 * 10**(26) #kg, massa aarde
-massa_mercurius = 3.285 * 10**(23) #kg, massa mercurius
-massa_venus = 4.867 * 10**(24) #kg, massa venus
-massa_mars = 6.39 * 10**(23) #kg, massa mars
-massa_jupiter = 1.898 * 10**(27) #kg, massa jupiter
-massa_saturnus = 5.682 * 10**(26) #kg, massa saturnus
-massa_uranus = 8.681 * 10**(25) #kg, massa Uranus
-massa_neptunus = 1.024 * 10**(26) #kg, massa neptunus
 
-# Initialize planet.
-initial_vector = np.array([1,1,1])
-initial_vel = np.array([1,0,0])
-earth = planet(mass=const.M_earth.value, initial_position=initial_vector, radius=const.R_earth.value, initial_velocity=initial_vel)
+if __name__ == "__main__":
+    # Example of 2D 2-body problem earth and sun
+
+    # Parameters for the simulator
+    time_frame = np.array([0, 365.25*24*3600], dtype=int)
+    step = 1000
+    method = 'RK23'
+    absolute_tolerance = 1e5
+    relative_tolerance = 1e4
+
+    # Masses
+    mass_earth = pdh.earth.mass
+    mass_sun = 1.989e30
+    mass = np.array([mass_earth, mass_sun], dtype=float)
+
+    # Initial conditions
+    au = 1.495e11
+    G = copy.deepcopy(const.G.value)
+    initial_position = [au, 0, 0, 0]
+    initial_velocity = [0, 2.9765e4, 0, 0]
+    initial_conditions = np.array(initial_position + initial_velocity, dtype=float)
+
+
+    # @njit
+    # def equation_of_speed(t, vec, mass, G):
+    #     length = int(len(vec)/2)
+    #     r = np.sqrt(vec[:length:2]**2+vec[1:length:2]**2)
+    #     x, y = vec[:length:2], vec[1:length:2]
+    #     a = np.zeros(length, dtype=float)
+    #
+    #     #maybe matrix voor for loop.
+    #     for i in range(1, int(length/2)):
+    #         # Calculation of V via gravitation force (Newtonian)
+    #         theta = np.arctan2((y-np.roll(y, i)), (x-np.roll(x, i)))
+    #
+    #         a[::2] = a[::2] - np.roll(mass, i)*G/np.abs(r-np.roll(r, i))**2 * np.cos(theta)
+    #         a[1::2] = a[1::2] - np.roll(mass, i)*G/np.abs(r-np.roll(r, i))**2 * np.sin(theta)
+    #
+    #     dv_total = a
+    #     d_total = vec[length:]
+    #
+    #     d_vec = np.concatenate((d_total, dv_total))
+    #     return d_vec
+    #
+    # # dy = equation_of_speed(time_frame[0], initial_conditions, mass, G)
+    # solution = solve_ivp(equation_of_speed, t_span=time_frame, y0=initial_conditions, args=(mass, G), max_step=step,
+    #                     method=method, rtol=relative_tolerance, atol=absolute_tolerance)
+    # data = solution['y']
+    # plt.figure()
+    # plt.plot(data[0], data[1])
+    # plt.plot(data[2], data[3])
+    # plt.gca().set_aspect('equal', adjustable='box')
+    # plt.show()
+    # print(dy)
+
