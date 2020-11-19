f4c7b5d7df4bfc9fa9870093af2c78917301b61d
Otto Broers <broers.otto@gmail.com>
2020-11-19 11:31:28 +0100

    genereren van plotjes geautomatiseerd voor animatie. https://clideo.com/editor/video-maker Deze sjizzle gebruik ik voor animatie generation.

--- CodePlaneetbanen.py
+++ CodePlaneetbanen.py
@@ -2,10 +2,12 @@ import numpy as np
 from scipy.integrate import solve_ivp
 import scipy as sp
 import matplotlib.pyplot as plt
+import matplotlib
 from mpl_toolkits.mplot3d import Axes3D
 from numba import njit
 import copy
 from data import PlanetaryDataHandler
+import time
 
 pdh = PlanetaryDataHandler()
 
@@ -32,10 +34,11 @@ def xyz(theta,eccentricity,smaxis,Omega,omega,I):
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
+
 def plot_elips_3d(theta,eccentricity,smaxis,Omega,omega,I, input):
     X,Y,Z,vector = xyz(theta,eccentricity,smaxis,Omega,omega, I)
     ax.plot(X,Y,Z, c=input)
-    plt.show()
+    #plt.show()
     return vector
 
 # Making plot for 0 to 2pi for Mercury
@@ -103,7 +106,7 @@ def ODE_solv(smaxis1,smaxis2,m_1,m_2,m_c,e1,e2,omega1,omega2,I1,I2,Omega1,Omega2
     IC = initial_conditions(e1,e2,omega1,omega2,I1,I2,Omega1,Omega2)
     return solve_ivp(sys_of_eq,[0,time], IC, args= args(smaxis1,smaxis2,m_1,m_2,m_c),t_eval=t_ev, dense_output=True)
 
-# Transforming the variables back to usual coordinate system
+# Transforming the variables back to usual coordinate system, returns e,I,omega,Omega
 def variable_transfromations(h,k,p,q):
     return np.array([np.sqrt(h**2+k**2), np.sqrt(p**2+q**2),
                     np.arcsin(h/np.sqrt(h**2+k**2)), np.arcsin(p/np.sqrt(p**2+q**2))])
@@ -114,19 +117,48 @@ def variable_transfromations(h,k,p,q):
 
 # Probeersel voor het vinden van de juiste parameters
 
+
+#tijd = 3*10**14*365.25*24*60*60
+tijd = 3*10**16*365.25*24*60*60
 results = ODE_solv(m.smaxis,v.smaxis,m.mass,v.mass,pdh.sun.mass,0.2056,0.006772,
          m.loanode,v.loanode,np.deg2rad(m.orbital_inclination),np.deg2rad(v.orbital_inclination),
-         m.argperiapsis,v.argperiapsis, time = 10**9*365.25*24*60*60, t_ev = None)
+         m.argperiapsis,v.argperiapsis, time = tijd, t_ev = None)
 
 
-t = np.linspace(0,10**9*365.25*24*60*60,10)
+t = np.linspace(0,tijd,500)
 z=results.sol(t)
 
 # Kijken of de parameters op t=1000 sense maken in het originele coordinate system
+
 original_system_mercury = variable_transfromations(z.T[-1][0], z.T[-1][2],
                                                    z.T[-1][4], z.T[-1][6])
 
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+theta_values = np.linspace(0,2*np.pi,10**3)
+#matplotlib.use('Agg')
+plt.ioff()
+for i in range(np.size(z[0])):
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    # ax.set_zticks([-10**10, 0, 10**10])
+    # ax.set_xticks([-10**10, 0, 10**10])
+    # ax.set_yticks([-10**10, 0, 10**10])
+    plt.xlim([-2*10**11, 2*10**11])
+    plt.ylim([-2*10**11, 2*10**11])
+    ax.set_zlim([-2*10**10, 2*10**10])
 
+    original_system_mercury = variable_transfromations(z.T[i][0], z.T[i][2],
+                                                       z.T[i][4], z.T[i][6])
+    original_system_venus = variable_transfromations(z.T[i][1], z.T[i][3],
+                                                       z.T[i][5], z.T[i][7])
 
+    plot_elips_3d(theta_values,original_system_mercury[0],m.smaxis,original_system_mercury[3],original_system_mercury[2],original_system_mercury[1],'r')
+    plot_elips_3d(theta_values,original_system_venus[0],v.smaxis,original_system_venus[3],original_system_venus[2],original_system_venus[1],'grey')
+    ax.plot(0,0,0,'ko')
+    plt.savefig('animatie/testje{}.png'.format(i))
+    plt.close(fig)
+
+    #plt.close(fig)
 
 
