1c35ea4fa2a4c57e9068a579eba74144f047ebb4
ShayanFarrokhi <71279533+ShayanFarrokhi@users.noreply.github.com>
2020-11-21 12:48:12 +0100

    bezig met beta

--- N-bodyproblem.py
+++ N-bodyproblem.py
@@ -1,6 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from data import PlanetaryDataHandler
-
+from scipy.integrate import solve_ivp
+from scipy.integrate import quad
 # Imporing all the important planetary data
 pdh = PlanetaryDataHandler()
 j=pdh.jupiter
@@ -43,7 +45,17 @@ def alpha_calculator(semi_major):
     # Calculating_alpha_bar elements for neptunus
     alpha_bar_neptunus = alpha_neptunus_total
 
-    # Making matrix of these elements:
+    # Making matrix of these elements for alpha:
+    zero_matrix = np.zeros((4,4))
+    zero_matrix[0,1:] = alpha_jupiter
+    zero_matrix[1][0] = alpha_saturnus_internal
+    zero_matrix[1,2:] = alpha_saturnus_exeternal
+    zero_matrix[2,0:2] = alpha_uranus_internal
+    zero_matrix[2][3] = alpha_uranus_exeternal
+    zero_matrix[3,0:3] = alpha_neptunus_total
+    alpha_matrix = zero_matrix
+
+    # Making matrix of these elements for alpha_times_bar:
     zero_matrix = np.zeros((4,4))
     zero_matrix[0,1:] = alpha_jupiter * alpha_bar_jupiter
     zero_matrix[1][0] = alpha_saturnus_internal * alpha_bar_saturn[:1]
@@ -53,8 +65,36 @@ def alpha_calculator(semi_major):
     zero_matrix[3,0:3] = alpha_neptunus_total * alpha_bar_neptunus
     alpha_times_bar_matrix = zero_matrix
 
-    return alpha_times_bar_matrix
+    return alpha_matrix, alpha_times_bar_matrix
 
-alpha_matrix = alpha_calculator(alpha_vector)
+alpha_matrix,alpha_times_bar_matrix = alpha_calculator(alpha_vector)
 
-#
+# n_vector, needed for calculating A_jj, A_jk etc later on
+n_vector = 2*np.pi/(alpha_vector**(3/2))
+
+# Adjusting the alpha_matrix in a slightly better form for doing calculations later on
+alpha_times_bar_matrix[0,:]=alpha_times_bar_matrix[0,:] * n_vector[0]
+alpha_times_bar_matrix[1,:] = alpha_times_bar_matrix[1,:]  * n_vector[1]
+alpha_times_bar_matrix[2,:] = alpha_times_bar_matrix[2,:]  * n_vector[2]
+alpha_times_bar_matrix[3,:] = alpha_times_bar_matrix[3,:]  * n_vector[3]
+
+# The b_3/2 elements
+def integrand1(phi, alpha):
+    return (1 / np.pi) * np.cos(phi) / (1 - 2 * alpha_matrix[0,:] *
+                                        np.cos(phi) + alpha ** 2) ** (3/2) # angle phi
+
+def integrand2(phi,alpha):
+    return (1 / np.pi) * np.cos(2 * phi) / (1 - 2 * alpha_matrix[0,:] *
+                                            np.cos(2 * phi) + alpha_matrix[1,:] ** 2) ** (3 / 2)  # angle 2*phi
+
+def beta1(alpha):
+    return quad(integrand1, 0, 2*np.pi, args=(alpha))[0]
+
+def beta2(alpha):
+    return quad(integrand2, 0, 2*np.pi, args=(alpha))[0]
+
+vec_beta1 = np.vectorize(beta1)
+vec_beta2 = np.vectorize(beta2)
+
+# Making the Beta Matrix
+
